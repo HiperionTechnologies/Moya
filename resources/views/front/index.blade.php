1f0e9be954eb3f85bc68ca4b502795ad4feede96
jotaeles <jl_sandoval_salas@hotmail.com>
2019-03-17 12:54:19 -0600

    done styles for login sedes

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,34 +5,42 @@
     <meta http-equiv="x-ua-compatible" content="ie-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, srink-to-fit=no" >
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'MOYA') }}</title>
-    
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
 
-    <!--style-->
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,500,700" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+
 </head>
 <body>
-    <div id="app">
-        {!!Form::open(['route'=>'sede.redirect','method'=>'POST'])!!}
-		<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
-			<div class="form-group">
-				{!!Form::select('sede',$sedes,null,['class'=>'form-control','placeholder'=>'Sedes'])!!}
-			</div>
-		</div>
-		<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
-			<div class="form-group">
-				{!!Form::submit('Entrar',['class'=>'btn btn-primary'])!!}
-			</div>
-		</div>
-		{!!Form::close()!!}
-    </div>
+    <main id="m-select-sede" class="m-select-sede">
+        <div class="m-select-sede-bg" style="background-image:url({{ asset('images/bg_login.jpg') }})">
+            <div class="m-select-sede-shape"></div>
+        </div>
+        <div class="container">
+            <div class="m-select-card">
+                <div class="m-select-card-header">
+                    <div class="m-select-card-brand">
+                        <img src="{{ asset('images/logo_moya.svg') }}" alt="Logotipo Moya">
+                    </div>
+                    <p class="m-select-card-title">Selecciona tu sede</p>
+                </div>
+                <div class="m-select-card-body">
+                    {!!Form::open(['route'=>'sede.redirect','method'=>'POST'])!!}
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            {!!Form::select('sede',$sedes,null,['class'=>'form-control m-form-select','placeholder'=>'Sedes'])!!}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 text-center">
+                        <div class="form-group">
+                            {!!Form::submit('Entrar',['class'=>'btn m-btn-black'])!!}
+                        </div>
+                    </div>
+                    {!!Form::close()!!}
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
